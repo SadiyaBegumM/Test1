a04fc440dcc94404fb5c20f71d97e4d450197ac2
SadiyaBegumM <sadiyasaji98@gmail.com>
2021-10-03 15:13:11 +0530

    Name Age

--- src/org/test1/TestCase1.java
+++ src/org/test1/TestCase1.java
@@ -1,11 +1,24 @@
 package org.test1;
 
 public class TestCase1 {
-	
+
+
+
+
+	private void name() {
+		System.out.println("Sadiya");
+
+
+	}
+	private void age() {
+		System.out.println("23");
+	}	
+
 	public static void main(String[] args) {
-		
-		
-		System.out.println("Sadiya Begum M");
+		TestCase1 t = new TestCase1();
+		t.name();
+		t.age();
+
 	}
 
 }
